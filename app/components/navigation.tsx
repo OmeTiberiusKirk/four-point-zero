8cd6a7b1ab3c218860d2bc48255ca162efe2232b
nukool <nukool@40.co.th>
2026-01-07 05:28:37 +0700

    ~

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -6,12 +6,16 @@ import { Bars } from "./icons";
 export function Navigation() {
   return (
     <div className={["sticky", "top-0", "flex", "justify-end"].join(" ")}>
-      <Button className={["rounded-none", "text-lg", "font-bold"].join(" ")}>
+      <Button
+        className={["rounded-none", "text-2xl", "font-bold"].join(" ")}
+        size="lg"
+      >
         Contact Us
       </Button>
       <Button
         className={["rounded-none", "border-none", "shadow-none"].join(" ")}
         variant="outline"
+        size="lg"
       >
         <Bars />
       </Button>
