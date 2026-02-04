6bb1a2d5754d89b4da79d338ebbc70fc7748e6ff
OmeTiberiusKirk <nukool@40.co.th>
2026-02-04 11:33:48 +0700

    ~

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={cn("dark")}>
+    <html lang="en" className={cn("")}>
       <body
         className={cn([
           "antialiased",
